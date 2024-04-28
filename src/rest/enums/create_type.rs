8b2e641b20658a20dc4ea5ada0e5f6316162d4f0
Cafer Can Gündoğdu <cafercangundogdu@gmail.com>
2024-04-28 15:41:57 +0300

    feat(enums): create type impl

---
+++ src/rest/enums/create_type.rs
@@ -0,0 +1,89 @@
+use std::fmt::{Display, Formatter, Result};
+
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize, PartialEq)]
+pub enum CreateType {
+    // https://bybit-exchange.github.io/docs/v5/enum#createtype
+    #[serde(rename = "CreateByUser")]
+    CreateByUser,
+    #[serde(rename = "CreateByAdminClosing")]
+    CreateByAdminClosing,
+    #[serde(rename = "CreateByStopOrder")]
+    CreateByStopOrder, // Futures conditional order
+    #[serde(rename = "CreateByTakeProfit")]
+    CreateByTakeProfit, // Futures take profit order
+    #[serde(rename = "CreateByPartialTakeProfit")]
+    CreateByPartialTakeProfit, // Futures partial take profit order
+    #[serde(rename = "CreateByStopLoss")]
+    CreateByStopLoss, // Futures stop loss order
+    #[serde(rename = "CreateByPartialStopLoss")]
+    CreateByPartialStopLoss, // Futures partial stop loss order
+    #[serde(rename = "CreateByTrailingStop")]
+    CreateByTrailingStop, // Futures trailing stop order
+    #[serde(rename = "CreateByLiq")]
+    CreateByLiq, // Laddered liquidation to reduce the required maintenance margin
+    #[serde(rename = "CreateByTakeOver_PassThrough")]
+    CreateByTakeOver_PassThrough, // If the position is still subject to liquidation (i.e., does not meet the required maintenance margin level), the position shall be taken over by the liquidation engine and closed at the bankruptcy price.
+    #[serde(rename = "CreateByAdl_PassThrough")]
+    CreateByAdl_PassThrough, // Auto-Deleveraging(ADL)
+    #[serde(rename = "CreateByBlock_PassThrough")]
+    CreateByBlock_PassThrough, // Order placed via Paradigm
+    #[serde(rename = "CreateByBlockTradeMovePosition_PassThrough")]
+    CreateByBlockTradeMovePosition_PassThrough, // Order created by move position
+    #[serde(rename = "CreateByClosing")]
+    CreateByClosing, // The close order placed via web or app position area - web/app
+    #[serde(rename = "CreateByFGridBot")]
+    CreateByFGridBot, // Order created via grid bot - web/app
+    #[serde(rename = "CloseByFGridBot")]
+    CloseByFGridBot, // Order closed via grid bot - web/app
+    #[serde(rename = "CreateByTWAP")]
+    CreateByTWAP, // Order created by TWAP - web/app
+    #[serde(rename = "CreateByTVSignal")]
+    CreateByTVSignal, // Order created by TV webhook - web/app
+    #[serde(rename = "CreateByMmRateClose")]
+    CreateByMmRateClose, // Order created by Mm rate close function - web/app
+    #[serde(rename = "CreateByMartingaleBot")]
+    CreateByMartingaleBot, // Order created by Martingale bot - web/app
+    #[serde(rename = "CloseByMartingaleBot")]
+    CloseByMartingaleBot, // Order closed by Martingale bot - web/app
+    #[serde(rename = "CreateByIceBerg")]
+    CreateByIceBerg, // Order created by Ice berg strategy - web/app
+    #[serde(rename = "CreateByArbitrage")]
+    CreateByArbitrage, // Order created by arbitrage - web/app
+    #[serde(rename = "CreateByDdh")]
+    CreateByDdh, // Option dynamic delta hedge order - web/app
+}
+
+impl Display for CreateType {
+    fn fmt(&self, f: &mut Formatter) -> Result {
+        match self {
+            CreateType::CreateByUser => write!(f, "CreateByUser"),
+            CreateType::CreateByAdminClosing => write!(f, "CreateByAdminClosing"),
+            CreateType::CreateByStopOrder => write!(f, "CreateByStopOrder"),
+            CreateType::CreateByTakeProfit => write!(f, "CreateByTakeProfit"),
+            CreateType::CreateByPartialTakeProfit => write!(f, "CreateByPartialTakeProfit"),
+            CreateType::CreateByStopLoss => write!(f, "CreateByStopLoss"),
+            CreateType::CreateByPartialStopLoss => write!(f, "CreateByPartialStopLoss"),
+            CreateType::CreateByTrailingStop => write!(f, "CreateByTrailingStop"),
+            CreateType::CreateByLiq => write!(f, "CreateByLiq"),
+            CreateType::CreateByTakeOver_PassThrough => write!(f, "CreateByTakeOver_PassThrough"),
+            CreateType::CreateByAdl_PassThrough => write!(f, "CreateByAdl_PassThrough"),
+            CreateType::CreateByBlock_PassThrough => write!(f, "CreateByBlock_PassThrough"),
+            CreateType::CreateByBlockTradeMovePosition_PassThrough => {
+                write!(f, "CreateByBlockTradeMovePosition_PassThrough")
+            }
+            CreateType::CreateByClosing => write!(f, "CreateByClosing"),
+            CreateType::CreateByFGridBot => write!(f, "CreateByFGridBot"),
+            CreateType::CloseByFGridBot => write!(f, "CloseByFGridBot"),
+            CreateType::CreateByTWAP => write!(f, "CreateByTWAP"),
+            CreateType::CreateByTVSignal => write!(f, "CreateByTVSignal"),
+            CreateType::CreateByMmRateClose => write!(f, "CreateByMmRateClose"),
+            CreateType::CreateByMartingaleBot => write!(f, "CreateByMartingaleBot"),
+            CreateType::CloseByMartingaleBot => write!(f, "CloseByMartingaleBot"),
+            CreateType::CreateByIceBerg => write!(f, "CreateByIceBerg"),
+            CreateType::CreateByArbitrage => write!(f, "CreateByArbitrage"),
+            CreateType::CreateByDdh => write!(f, "CreateByDdh"),
+        }
+    }
+}
--- src/rest/enums/mod.rs
+++ src/rest/enums/mod.rs
@@ -1,6 +1,7 @@
 pub mod announcement_tag;
 pub mod announcement_type;
 pub mod category;
+pub mod create_type;
 pub mod interval_time;
 pub mod locale;
 pub mod option_period;
