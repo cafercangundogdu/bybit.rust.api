8d281a0a89e4d84daf29f4564a45e479a5f66376
Cafer Can Gündoğdu <cafercangundogdu@gmail.com>
2024-05-05 21:27:09 +0300

    feat(errors): create error.json

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,12 @@ mod handlers;
 mod rest;
 mod utils;
 
+mod generated {
+    include!(concat!(env!("OUT_DIR"), "/error_codes.rs"));
+}
+
+use generated::{ErrorCodes, ErrorTypes};
+
 fn main() {
     println!("bybit")
 }
